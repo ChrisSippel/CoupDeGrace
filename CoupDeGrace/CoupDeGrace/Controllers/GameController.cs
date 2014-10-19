0dc68869ccb40fbba704623cbbab99750cd5fdf4
Chris Sippel <Chris_Sippel@hotmail.com>
2014-10-19 17:22:22 -0400

    Played with needing to be logged in to access a web page

---
+++ CoupDeGrace/CoupDeGrace/Controllers/GameController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CoupDeGrace.Controllers
+{
+    public class GameController : Controller
+    {
+        //
+        // GET: /Game/
+
+        public ActionResult Index()
+        {
+            if (string.IsNullOrEmpty(User.Identity.Name))
+            {
+                return RedirectToAction("Login", "Account", Request.Url.ToString());
+            }
+            else
+            {
+                return View();
+            }
+        }
+    }
+}
